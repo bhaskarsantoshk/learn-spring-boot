fd19c77fc71354261947b32ef0c0c3376f66aab6
santosh.bhaskar <bhaskarsantoshk@gmail.com>
2019-10-05 18:47:44 +0530

    spring boot starter

---
+++ src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
@@ -0,0 +1,19 @@
+package io.javabrains.springbootstarter.topic;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TopicController {
+	@RequestMapping("/topics")
+	public List<Topic>  getAllTopics() {
+		return Arrays.asList(
+				new Topic("spting", "Spring Framework", "Spring Framework Desc"),
+				new Topic("java", "Core Java", "Core Java Desc"),
+				new Topic("javascript", "JavaScript", "JavaScript Description")
+				);
+	}
+}
